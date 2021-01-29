6870bb321e4ec64010f12a85d69afb939ae70b17
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2021-01-30 01:24:26 +0600

    Completed the 2.3.2 task

--- student/02/factors/main.cpp
+++ student/02/factors/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
+    int number;
     cout << "Enter a positive number: ";
+    cin>> number1;
+    if (number>=0){
+        int squareRoot = sqrt(number);
+        while(number%squareRoot!=0){
+            squareRoot--;
+        }
+        cout<<number<<" = "<<squareRoot<<" * "<<number/squareRoot;
+    }
+    else{
+        cout << "Only positive numbers accepted";
+    }
 
     return 0;
 }
