6d0c9f49b1865871b531239250a96ca4e7dedd38
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2021-01-29 23:11:17 +0600

    Reading the input and printing the numbers

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
+using namespace std;
 
 int main()
 {
-    std::cout << "How many numbers would you like to have? ";
+    cout << "How many numbers would you like to have? ";
+    int number;
+    cin >> number;
+    for(int i=1;i<=number;i++){
+        cout<<i<<endl;
+    }
 }
