3546da7ba7ddac688664fe3da58b348f5cd522fc
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2021-01-31 22:26:00 +0600

    Solved simple_library Try 1

--- student/03/simple_library/book.cpp
+++ student/03/simple_library/book.cpp
@@ -4,13 +4,44 @@
 
 using namespace std;
 
-Book::Book(string name, string author, bool loan= false):name_(name), author_(author), loan_(loan){
+Book::Book(string name, string author, bool loan):name_(name), author_(author), loan_(loan){
 
 }
 
 void Book::print() const{
-    if(_loan==false){
+    if(loan_==false){
         cout<< name_ << " : " << author_ << endl;
         cout<< "- available"<<endl;
     }
+    else{
+        cout<< name_ << " : " << author_ << endl;
+        cout<< "- loaned: ";
+        loanDate_.print();
+        cout<< "- to be returned: ";
+        returnDate_.print();
+    }
+}
+
+void Book::loan(Date x){
+    if(loan_==true){
+        cout<<"Already loaned: cannot be loaned"<<endl;
+    }
+    else{
+        loanDate_=x;
+        x.advance(28);
+        returnDate_=x;
+        loan_= true;
+    }
+   
+}
+
+void Book::renew(){
+    if(loan_==true){
+        returnDate_.advance(28);
+}
+    }
+   
+
+void Book::give_back(){
+    loan_= false;
 }
--- student/03/simple_library/book.hh
+++ student/03/simple_library/book.hh
@@ -2,6 +2,7 @@
 #define BOOK_HH
 
 #include <string>
+#include"date.hh"
 using namespace std;
 
 class Book
@@ -9,10 +10,14 @@ class Book
 public:
     Book(string name, string author, bool loan= false);
     void print() const;
+    void loan(Date x);
+    void renew();
+    void give_back();
 
 private:
     string name_,author_;
     bool loan_;
+    Date loanDate_,returnDate_;
 };
 
 #endif 
