e47ec41add37e8b6608e6f4d75250786e9eea7fa
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2021-02-01 00:33:07 +0600

    Started working on Molkky

---
+++ student/03/molkky/player.hh
@@ -0,0 +1,16 @@
+#ifndef PLAYER_HH
+#define PLAYER_HH
+
+#include <iostream>
+#include <string>
+ 
+using namespace std; 
+
+class Player{
+    public:
+        Player();
+    private:
+
+};
+
+#endif
