3207c7c54002d922109208eba0ded7bdad088e3d
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2021-02-13 13:50:05 +0600

    Completed line numbers

---
+++ student/05/line_numbers/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream> 
+#include <string>
+ 
+using namespace std;
+ 
+int main() {
+    string filename = "";
+    string outputFileName= "";
+    cout << "Input file: ";
+    getline(cin, filename);
+    cout << "Output file: ";
+    getline(cin, outputFileName);
+ 
+    ifstream file_object(filename);
+    ofstream outputFile_object(outputFileName);
+    if ( not file_object ) {
+        cout << "Error! The file "<< filename <<" cannot be opened." << endl;
+        return EXIT_FAILURE;
+    } else {
+        string line;
+        int lineCounter=1;
+        while ( getline(file_object, line) ) {
+           outputFile_object << lineCounter << " " << line << endl;
+           lineCounter+=1;
+        }
+        file_object.close();
+    }
+
+    return EXIT_SUCCESS;
+}
