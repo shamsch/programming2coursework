d50910c002c0c8c87d52c14c9dec1be2c2f52ba9
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2021-02-13 14:26:47 +0600

    Another try to fix line_numbers

--- student/05/line_numbers/main.cpp
+++ student/05/line_numbers/main.cpp
@@ -13,11 +13,11 @@ int main() {
     getline(cin, outputFileName);
  
     ifstream file_object(filename);
-    ofstream outputFile_object(outputFileName);
     if ( not file_object ) {
         cout << "Error! The file "<< filename <<" cannot be opened." << endl;
         return EXIT_FAILURE;
     } else {
+        ofstream outputFile_object(outputFileName);
         string line;
         int lineCounter=1;
         while ( getline(file_object, line) ) {
@@ -25,9 +25,10 @@ int main() {
            lineCounter+=1;
         }
         file_object.close();
+        outputFile_object.close();
     }
-    
-    outputFile_object.close();
+
+   
 
     return EXIT_SUCCESS;
 }
