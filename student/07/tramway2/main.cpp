fb3929c2216508c72e6056f916126a56db3ffeab
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2021-02-28 18:40:22 +0600

    Algorithm implemented

--- student/07/tramway2/main.cpp
+++ student/07/tramway2/main.cpp
@@ -17,13 +17,12 @@
 #include <map>
 #include <utility>
 #include <fstream>
+#include <cmath>
 
 using namespace std;
-using dataStrucutre = map<string, vector<pair<string, string>>>;
+using dataStrucutre = map<string, vector<pair<string, float>>>;
 const string ERROR = "Error: Invalid input.";
 
-// The most magnificent function in this whole program.
-// Prints a RASSE
 void print_rasse()
 {
     cout << "=====//==================//===\n"
@@ -38,7 +37,7 @@ void print_rasse()
          << endl;
 }
 
-bool checkIfAPairExist(vector<pair<string, string>> &vec, string &name, string &dist)
+bool checkIfAPairExist(vector<pair<string, float>> &vec, string &name, float &dist, bool avoidDistance = false)
 {
     for (auto ele : vec)
     {
@@ -46,7 +45,7 @@ bool checkIfAPairExist(vector<pair<string, string>> &vec, string &name, string &
         {
             return false;
         }
-        if (ele.second == dist)
+        if (ele.second == dist && !(avoidDistance))
         {
             return false;
         }
@@ -94,7 +93,7 @@ bool readInputFile(dataStrucutre &cont)
         {
             vector<string> temp;
             temp.clear();
-            temp = split(line, ';');
+            temp = split(line, ';', true);
             if (temp.size() == 3 || temp.size() == 2)
             {
                 string lineName = temp.at(0);
@@ -103,13 +102,13 @@ bool readInputFile(dataStrucutre &cont)
                 {
                     temp.push_back("0");
                 }
-                string distance = temp.at(2);
-                // cout<<lineName << " "<<nameOfStop<<" "<< distance << endl;
+                float distance = stof(temp.at(2));
+                //cout<<lineName << " "<<nameOfStop<<" "<< distance << endl;
                 if (cont.find(lineName) == cont.end())
                 {
                     // line doesn't exist
-                    pair<string, string> tempPair(nameOfStop, distance);
-                    vector<pair<string, string>> tempVec;
+                    pair<string, float> tempPair(nameOfStop, distance);
+                    vector<pair<string, float>> tempVec;
                     tempVec.clear();
                     tempVec.push_back(tempPair);
                     cont.insert({lineName, tempVec});
@@ -119,7 +118,7 @@ bool readInputFile(dataStrucutre &cont)
                     //line exists already
                     if (checkIfAPairExist(cont[lineName], nameOfStop, distance))
                     {
-                        pair<string, string> tempPair(nameOfStop, distance);
+                        pair<string, float> tempPair(nameOfStop, distance);
                         cont[lineName].push_back(tempPair);
                     }
                     else
@@ -143,13 +142,230 @@ bool readInputFile(dataStrucutre &cont)
 void printLines(dataStrucutre &cont)
 {
     cout << "All tramlines in alphabetical order:" << endl;
+    vector<string> allLines;
+    allLines.clear();
     for (auto element : cont)
     {
-        cout << element.first << endl;
+        allLines.push_back(element.first);
+    }
+    sort(allLines.begin(),allLines.end());
+    for(auto line: allLines){
+        cout<<line<<endl;
     }
 }
 
-// Short and sweet main.
+bool sortbysec(const pair<string, float> &a, const pair<string, float> &b)
+{
+    return b.second > a.second;
+}
+
+void printStopsOfOneLine(dataStrucutre &cont, string &line)
+{
+    cout << "Line " << line << " goes through these stops in the order they are listed:" << endl;
+    sort(cont[line].begin(), cont[line].end(), sortbysec);
+    for (auto element : cont[line])
+    {
+        cout << " - " << element.first << " : " << element.second << endl;
+    }
+}
+
+bool doesThisLineExist(dataStrucutre &cont, string &line)
+{
+    if (cont.find(line) == cont.end())
+    {
+        //not found
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+void printAllStops(dataStrucutre &cont)
+{
+    vector<string> stops;
+    stops.clear();
+    for (auto vectors : cont)
+    {
+        for (auto pairs : vectors.second)
+        {
+            if (find(stops.begin(), stops.end(), pairs.first) == stops.end())
+            {
+                stops.push_back(pairs.first);
+            }
+        }
+    }
+    sort(stops.begin(), stops.end());
+    cout << "All stops in alphabetical order:" << endl;
+    for (auto name : stops)
+    {
+        if(name!=""){
+            cout << name << endl;
+        }
+    }
+}
+
+void findAllLinesWithAStop(dataStrucutre &cont, string &stop)
+{
+    vector<string> lines;
+    for (auto vectors : cont)
+    {
+        for (auto pairs : vectors.second)
+        {
+            if (pairs.first == stop)
+            {
+                lines.push_back(vectors.first);
+            }
+        }
+    }
+    if (lines.size())
+    {
+        cout << "Stop " << stop << " can be found on the following lines:" << endl;
+        for (auto element : lines)
+        {
+            cout << " - " << element << endl;
+        }
+    }
+    else
+    {
+        cout << "Error: Stop could not be found." << endl;
+    }
+}
+
+void calculateDistance(dataStrucutre &cont, string &line, string &firstStation, string &secondStation)
+{
+    float none = 0.0;
+    float firstDist = 0.0;
+    float secondDist = 0.0;
+    if (cont.find(line) != cont.end())
+    {
+        if (!(checkIfAPairExist(cont[line], firstStation, none, true) && checkIfAPairExist(cont[line], secondStation, none, true)))
+        {
+            for (auto ele : cont[line])
+            {
+                if (ele.first == firstStation)
+                {
+                    firstDist = ele.second;
+                }
+                if (ele.first == secondStation)
+                {
+                    secondDist = ele.second;
+                }
+            }
+            cout << "Distance between " << firstStation << " and " << secondStation << " is " << abs(firstDist - secondDist) << endl;
+        }
+        else
+        {
+            cout << "Error: Stop could not be found." << endl;
+        }
+    }
+    else
+    {
+        cout << "Error: Line could not be found." << endl;
+    }
+}
+
+string trimTheQuotationMark(vector<string> &listOfName)
+{
+    string result = "";
+    string firstElement = listOfName.at(0);
+    int count = 0;
+    for (auto &element : listOfName)
+    {
+        string temp = element;
+        if (temp[0] == '"' || temp[temp.length() - 1] == '"')
+        {
+            vector<string> tempVec;
+            tempVec = split(temp, '"', true);
+            result += tempVec.at(0) + " ";
+        }
+        else
+        {
+            result += temp + " ";
+        }
+        count++;
+        //last quotation mark found
+        if (temp[temp.length() - 1] == '"')
+        {
+            break;
+        }
+        //no quotation mark in first element
+        if (temp[0] != '"' && temp == firstElement)
+        {
+            break;
+        }
+    }
+    listOfName.erase(listOfName.begin(), listOfName.begin() + count);
+    result.pop_back();
+    return result;
+}
+
+void addNewLine(dataStrucutre &cont, string &lineName)
+{
+    if (cont.find(lineName) != cont.end())
+    {
+        //line already exists
+        cout << "Error: Stop/line already exists." << endl;
+    }
+    else
+    {
+        //line does NOT exist
+        vector<pair<string, float>> tempVec;
+        tempVec.clear();
+        cont.insert({lineName, tempVec});
+        cout << "Line was added." << endl;
+    }
+}
+
+void addStopToALine(dataStrucutre &cont, string &line, string &stopName, float &dist)
+{
+    if (cont.find(line) != cont.end())
+    {
+        if (checkIfAPairExist(cont[line], stopName, dist))
+        {
+            pair<string, float> tempPair(stopName, dist);
+            cont[line].push_back(tempPair);
+            cout << "Stop was added." << endl;
+        }
+        else
+        {
+            cout << "Error: Stop/line already exists." << endl;
+        }
+    }
+    else
+    {
+        cout << "Error: Line could not be found." << endl;
+    }
+}
+
+void removeAStopFromAllLine(dataStrucutre &cont, string &stopName)
+{
+    bool removedAtLeastOnce= false;
+    bool removeCondition= false;
+    pair<string, float> temp;
+    for (auto &map : cont)
+    {
+        for(auto &vec: map.second){
+            if(vec.first==stopName){
+                temp= vec;
+                removeCondition=true;
+                removedAtLeastOnce=true;
+            }
+        }
+        if(removeCondition){
+            remove(map.second.begin(),map.second.end(), temp);
+            removeCondition=false;
+        }
+    }
+    if(removedAtLeastOnce){
+        cout<<"Stop was removed from all lines."<<endl;
+    }
+    else{
+        cout<<"Error: Invalid input"<<endl;
+    }
+}
+
 int main()
 {
     print_rasse();
@@ -158,35 +374,116 @@ int main()
     {
         return EXIT_FAILURE;
     }
+
     while (true)
     {
-        std::string line;
-        std::cout << "tramway> ";
-        getline(std::cin, line);
-        std::vector<std::string> parts = split(line, ' ', true);
+        string line;
+        cout << "tramway> ";
+        getline(cin, line);
+        vector<string> parts = split(line, ' ', true);
 
-        std::string command = parts.at(0);
+        string command = parts.at(0);
 
         if (command == "LINES")
         {
-            if(parts.size()==1){
-                printLines(mainContainer);
+            printLines(mainContainer);
+        }
+        else if (command == "LINE")
+        {
+
+            if (parts.size() >= 2)
+            {
+                parts.erase(parts.begin());
+                string nameOfLine;
+                nameOfLine = trimTheQuotationMark(parts);
+                if (!doesThisLineExist(mainContainer, nameOfLine))
+                {
+                    cout << "Error: Line could not be found." << endl;
+                }
+                else
+                {
+                    printStopsOfOneLine(mainContainer, nameOfLine);
+                }
+            }
+            else
+            {
+                cout << ERROR << endl;
             }
         }
         else if (command == "STOPS")
         {
+            printAllStops(mainContainer);
+        }
+        else if (command == "STOP")
+        {
+            if (parts.size() >= 2)
+            {
+                parts.erase(parts.begin());
+                string stop = trimTheQuotationMark(parts);
+                // cout<<stop<<endl;
+                findAllLinesWithAStop(mainContainer, stop);
+            }
+            else
+            {
+                cout << ERROR << endl;
+            }
         }
         else if (command == "DISTANCE")
         {
+            parts.erase(parts.begin());
+            if (parts.size() >= 3)
+            {
+                string line = trimTheQuotationMark(parts);
+                string firstStation = trimTheQuotationMark(parts);
+                string secondStation = trimTheQuotationMark(parts);
+                // cout<<line<< " "<< firstStation << " " << secondStation << endl;
+                calculateDistance(mainContainer, line, firstStation, secondStation);
+            }
+            else
+            {
+                cout << ERROR << endl;
+            }
         }
         else if (command == "ADDLINE")
         {
+            if (parts.size() >= 2)
+            {
+                parts.erase(parts.begin());
+                string nameOfTheLine = trimTheQuotationMark(parts);
+                addNewLine(mainContainer, nameOfTheLine);
+            }
+            else
+            {
+                cout << ERROR << endl;
+            }
         }
         else if (command == "ADDSTOP")
         {
+            if (parts.size() >= 4)
+            {
+                parts.erase(parts.begin());
+                string line = trimTheQuotationMark(parts);
+                string station = trimTheQuotationMark(parts);
+                float dist = stof(trimTheQuotationMark(parts));
+                addStopToALine(mainContainer, line, station, dist);
+            }
+            else
+            {
+                cout << ERROR << endl;
+            }
         }
         else if (command == "REMOVE")
         {
+            if (parts.size() >= 2)
+            {
+                parts.erase(parts.begin());
+                string station = trimTheQuotationMark(parts);
+                removeAStopFromAllLine(mainContainer, station);
+            }
+            else
+            {
+                cout << ERROR << endl;
+            }
         }
         else if (command == "QUIT")
         {
@@ -194,7 +491,7 @@ int main()
         }
         else
         {
-            cout<<ERROR<<endl;
+            cout << ERROR << endl;
         }
     }
 
