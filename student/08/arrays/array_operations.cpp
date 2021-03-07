89929908ebbd361c0136d1ea8f95fdbcf718e078
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2021-03-07 16:49:36 +0600

    array_operation try 1

--- student/08/arrays/array_operations.cpp
+++ student/08/arrays/array_operations.cpp
@@ -1,1 +1,42 @@
 #include "array_operations.hh"
+#include<iostream> 
+#include<algorithm> 
+
+int greatest_v1(int* itemptr, int size) {
+    int max= 0;
+    int counter=0;
+    while(counter<size){
+        max=std::max(max, *itemptr);
+        itemptr++;
+        counter++;
+    }
+    return max;
+}
+
+int greatest_v2(int* itemptr, int* endptr) {
+    int max=0;
+    while(itemptr<endptr){
+        max=std::max(max, *itemptr);
+        itemptr++;
+    }
+    return max;
+}
+
+void copy(int* itemptr, int* endptr, int* targetptr) {
+    while(itemptr<endptr){
+        *targetptr=*itemptr;
+        targetptr++;
+        itemptr++;
+    }
+}
+
+void reverse(int* leftptr, int* rightptr) {
+    rightptr-=1;
+    while(leftptr<rightptr){
+        int leftVal=*leftptr;
+        *leftptr=*rightptr;
+        *rightptr=leftVal;
+        ++leftptr;
+        --rightptr;
+    }
+}
