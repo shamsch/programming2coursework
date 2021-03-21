86d1cc791ef3390719a3fb966aae0d6c55b114d9
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2021-03-21 17:38:24 +0600

    Complete reverse, try 1

--- student/10/reverse/cards.cpp
+++ student/10/reverse/cards.cpp
@@ -2,22 +2,23 @@
 #include <memory>
 #include "cards.hh"
 
-
-Cards::Cards(): top_( nullptr ) {
+Cards::Cards()
+{
 }
 
-
-void Cards::add(int id) {
-    std::shared_ptr<Card_data> new_card 
-            = std::make_shared<Card_data>(Card_data{id, top_});
-    top_ = new_card;
+void Cards::add(int id)
+{
+   std::shared_ptr<Card_data> new_card = std::make_shared<Card_data>(Card_data{id, top_});
+   top_ = new_card;
 }
 
-void Cards::print(std::ostream& s) {
+void Cards::print(std::ostream &s)
+{
    std::shared_ptr<Card_data> to_be_printed = top_;
    int nr = 1;
 
-   while( to_be_printed != 0 ) {
+   while (to_be_printed != 0)
+   {
       s << nr << ": " << to_be_printed->data << std::endl;
       to_be_printed = to_be_printed->next;
       ++nr;
@@ -29,3 +30,32 @@ void Cards::print(std::ostream& s) {
 // but open the file cards.hh and click the declaration of the method
 // by the right mouse button and select
 // Refactor > Add definition in cards.cpp
+
+bool Cards::remove(int &id)
+{
+   if (top_ != nullptr)
+   {
+      id = top_->data;
+      top_ = top_->next;
+      return true;
+   }
+   return false;
+}
+
+void Cards::reverse()
+{
+   std::shared_ptr<Card_data> current = top_;
+   std::shared_ptr<Card_data> previous = nullptr;
+   std::shared_ptr<Card_data> next = nullptr;
+
+   while (current != nullptr)
+   {
+      next= current->next;
+
+      current->next=previous;
+
+      previous=current;
+      current=next;
+   }
+   top_=previous;
+}
