4f56e4ab03dd7e85b1f1d8cbc5c8d63787772f22
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2021-04-14 16:47:07 +0600

    Implemented timer

--- student/12/timer/mainwindow.cpp
+++ student/12/timer/mainwindow.cpp
@@ -6,9 +6,54 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->lcdNumberMin->setPalette(Qt::red);
+    ui->lcdNumberSec->setPalette(Qt::green);
+    timer= new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(runTheClock()));
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_startButton_clicked()
+{
+    if(!startPressed){
+        timer->start(1000);
+    }
+    startPressed=true;
+}
+
+void MainWindow::runTheClock()
+{
+    if(secs<60){
+        ++secs;
+    }
+    else{
+        secs=0;
+        ++mins;
+    }
+    QString sec= QString::number(secs);
+    QString min= QString::number(mins);
+    ui->lcdNumberSec->display(sec);
+    ui->lcdNumberMin->display(min);
+}
+
+void MainWindow::on_stopButton_clicked()
+{
+    timer->stop();
+    startPressed=false;
+}
+
+void MainWindow::on_resetButton_clicked()
+{
+    on_stopButton_clicked();
+    secs=0;
+    mins=0;
+    QString sec= QString::number(secs);
+    QString min= QString::number(mins);
+    ui->lcdNumberSec->display(sec);
+    ui->lcdNumberMin->display(min);
 }
--- student/12/timer/mainwindow.hh
+++ student/12/timer/mainwindow.hh
@@ -24,9 +24,19 @@ public:
 private slots:
     // Add your slots here
 
+    void on_startButton_clicked();
+    void runTheClock();
+
+    void on_stopButton_clicked();
+
+    void on_resetButton_clicked();
+
 private:
     Ui::MainWindow *ui;
     // Add other necessary members here
+    unsigned int secs=0;
+    unsigned int mins=0;
+    bool startPressed=false;
 };
 
 #endif // MAINWINDOW_HH
