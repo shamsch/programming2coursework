df9d88c9c821939486a3bb919f56b1df556eea3a
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2021-05-04 16:36:23 +0600

    Made a semi-working version of the game

--- student/13/pairs_gui/mainwindow.cpp
+++ student/13/pairs_gui/mainwindow.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.hh"
 #include "ui_mainwindow.h"
+#include "dialog.hh"
+#include <QHBoxLayout>
+#include <QPoint>
+#include <QCursor>
+#include <QGraphicsView>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
 }
 
 MainWindow::~MainWindow()
@@ -37,5 +44,103 @@ void MainWindow::on_playerNameStringLineEdit_editingFinished()
     QString allPlayerString= ui->playerNameStringLineEdit->text();
     nameOfPlayer= allPlayerString.split(QString(" "));
     qDebug() << nameOfPlayer;
+}
+
+
+void MainWindow::validateData()
+{
+    if(amountOfCards%2==0 && amountOfCards!=0){
+        if(numberOfPlayers>=1){
+            if(nameOfPlayer.size()==numberOfPlayers){
+                ui->gameStatusTextBrowser->setText("");
+                findRowAndCol(amountOfCards,row,col);
+                makeBoardGUI(row,col);
+            }
+            else{
+                ui->gameStatusTextBrowser->setText("Error in players name, GAME CAN NOT START!");
+            }
+        }
+        else{
+            ui->gameStatusTextBrowser->setText("Error in number of players, GAME CAN NOT START!");
+        }
+    }
+    else{
+        ui->gameStatusTextBrowser->setText("Amount of cards not valid, GAME CAN NOT START!");
+    }
+}
+
+void MainWindow::makeARandomString(int n)
+{
+    char starter='a';
+    for(int i=0; i<n/2;i++){
+        randomString+= (starter+i);
+        randomString+= (starter+i);
+    }
+    std::random_shuffle(randomString.begin(),randomString.end());
+}
+
+void MainWindow::on_startGamePushButton_clicked()
+{
+    validateData();
+
+}
+
+void MainWindow::cardClicked()
+{
+    if(cardsOpen<2){
+        QPushButton *button = (QPushButton *) sender();
+         for(unsigned int i = 0; i < cardButton_.size(); ++i)
+         {
+             if(cardButton_.at(i)==button)
+             {
+                 QString text;
+                 text= buttonWithLetters[cardButton_.at(i)];
+                 button->setText(text);
+             }
+         }
+         openCards.push_back(button);
+         cardsOpen++;
+    }
+}
+
+void MainWindow::makeBoardGUI(int noOfRow, int noOfCol)
+{
+
+    gameBoard = new Dialog(this);
+
+    QWidget *widget= new QWidget(gameBoard);
+    QGridLayout *layout= new QGridLayout(widget);
+
+    for(int i=0;i<noOfRow;i++){
+        for(int j=0; j<noOfCol; j++){
+            QPushButton *button = new QPushButton("X",this);
+            cardButton_.push_back(button);
+            layout->addWidget(button, i,j);
+            connect(button, &QPushButton::clicked,
+                    this, &MainWindow::cardClicked);
+        }
+    }
+    QPushButton *nextTurn= new QPushButton("Next turn", this);
+    layout->addWidget(nextTurn, noOfRow, noOfCol/2);
+    gameBoard->show();
+    makeARandomString(amountOfCards);
+    for(unsigned long long int i=0; i<cardButton_.size(); i++){
+        buttonWithLetters.insert({cardButton_.at(i),randomString.at(i)});
+    }
+
+
+}
+
+void MainWindow::findRowAndCol(int number, int &row, int &col)
+{
+
+    for (int i = 1; i * i <= number; ++i)
+    {
+        if (number % i == 0)
+        {
+            row = i;
+        }
+    }
+    col = number / row;
 }
 
--- student/13/pairs_gui/mainwindow.hh
+++ student/13/pairs_gui/mainwindow.hh
@@ -3,6 +3,12 @@
 
 #include <QMainWindow>
 #include <QDebug>
+#include <vector>
+#include <QPushButton>
+#include <dialog.hh>
+#include <string>
+#include <algorithm>
+#include <map>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -25,11 +31,37 @@ private slots:
 
     void on_playerNameStringLineEdit_editingFinished();
 
+    void on_startGamePushButton_clicked();
+
+    void cardClicked();
+
 private:
     Ui::MainWindow *ui;
+    Dialog *gameBoard;
+
     int amountOfCards=0;
     int seedValue =0;
     int numberOfPlayers=0;
+    int row=0;
+    int col=0;
+    int cardsOpen=0;
+
     QStringList nameOfPlayer;
+
+    std::string randomString="";
+
+    std::vector<QPushButton*> cardButton_;
+    std::vector<QPushButton*> openCards;
+    std::map<QPushButton*, char> buttonWithLetters;
+
+    void makeBoardGUI(int noOfRow, int noOfCol);
+
+    void validateData();
+
+    void makeARandomString(int n);
+
+    void findRowAndCol(int number, int &row, int &col);
+
+    bool gameStarted=false;
 };
 #endif // MAINWINDOW_HH
